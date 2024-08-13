50bcaed0927eeb52746a0a6d07a6fd5348488b63
Knuckl3h3ad <knuckl3h3ad@outlook.com>
2024-08-12 18:20:35 -0700

    Added Products

--- app/page.js
+++ app/page.js
@@ -1,11 +1,39 @@
 "use client"
 
 import MainLayout from './layouts/Mainlayout'
+import CarouselComp from './components/CarouselComp'
+import Product from './components/Product'
 
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      title: "Brown Leather Bag",
+      description: "blah blah blah",
+      url: "https://picsum.photos/id/7",
+      price: 2500
+    },
+    {
+      id: 2,
+      title: "School Books",
+      description: "blah blah blah",
+      url: "https://picsum.photos/id/20",
+      price: 1999
+    }
+  ]
   return (
     <MainLayout>
-      Wow
+      <CarouselComp />
+
+      <div className='max-w-[1488px] mx-auto'>
+        <div className='text-2xl font-bold mt-4 mb-6 px-4'>Products</div>
+
+        <div className='grid grid-cols-5 gap-4'>
+          {products.map(product => (
+            <Product key={product.id} product={product}/>
+          ))}
+        </div>
+      </div>
     </MainLayout>
   );
 }
